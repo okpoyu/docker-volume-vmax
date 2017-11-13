215e90615e70e150b28ca7b7a5f9e0ba766f636a
Unana Okpoyo <unana.okpoyo@emc.com>
2017-11-13 12:25:31 +0000

    reverted systemd values back to default

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
+with open("requirements.txt") as requirements:
+    install_requires = requirements.readlines()
 
-setup(setup_requires=['pbr'],
-      pbr=True)
+setup(name='docker-volume-vmax',
+      version='1.0.0',
+      description='VMAX Docker Volume Plugin',
+      url='https://github.com/okpoyu/docker-volume-vmax',
+      author='Unana Okpoyo',
+      author_email='unana.okpoyo@dell.com',
+      license='Apache License, Version 2.0',
+      packages=find_packages(exclude=['docs', 'tests*']),
+      scripts=['bin/inq'],
+      install_requires=install_requires,
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Environment :: Plugins',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.0',
+          'Topic :: Internet :: WWW/HTTP',
+
+      ]
+      )
